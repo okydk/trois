c374a0190110d2a3558765a7d4372e61e4e98004
Stefan <stefan@oky.dk>
2021-01-09 16:18:53 +0100

    Global margin + font

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,10 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
 .app {
   .navbar {
     position: fixed;
